62bd3ee35f74a8c1e5bf2a5ca327dc96810a6273
Igorstar23 <larstar@ukr.net>
2022-06-25 16:58:08 +0300

    mod4

---
+++ mod4-solution/SpeakGoodBye.js
@@ -0,0 +1,9 @@
+(function(window) {
+    var byeSpeaker = {};
+    byeSpeaker.word = "Good Bye";
+    byeSpeaker.speak = function (name) {
+        console.log(byeSpeaker.word + " " + name);
+    }
+    window.byeSpeaker = byeSpeaker;
+})(window);
+
